9f2b5112092598c43b0d5fa6811157a1dd251024
NMD Tech <nmdtechnoloigies@gmail.com>
2026-07-22 20:50:00 +0530

    Changed gitdataloader

--- js/loaders/GitDataloader.js
+++ js/loaders/GitDataloader.js
@@ -2,6 +2,6 @@ console.log('New loader created at main');
 //edit loader
 const edit  = true;
 if (edit) {
-    console.log('Edit successful');
+    alert("Hi");
 }
 console.log('Test another commit without commit message');
