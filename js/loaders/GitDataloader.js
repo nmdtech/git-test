ca003ed1def6e131b7f3b4c82cb63c391d24bbe0
NMD Tech <nmdtechnoloigies@gmail.com>
2026-07-22 20:54:25 +0530

    Add confirm instead of alert

--- js/loaders/GitDataloader.js
+++ js/loaders/GitDataloader.js
@@ -2,6 +2,6 @@ console.log('New loader created at main');
 //edit loader
 const edit  = true;
 if (edit) {
-    alert("Hi");
+    confirm("What a surprise! How are you today??");
 }
 console.log('Test another commit without commit message');
